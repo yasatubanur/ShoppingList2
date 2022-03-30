6afc9de162292dcbada2591898cef23d7cd05f7c
Tuba Nur <yasatubanur@gmail.com>
2022-03-30 14:47:40 +0300

    userInterface coded

---
+++ ShoppingList2/DetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsViewController.swift
+//  ShoppingList2
+//
+//  Created by Tuba Nur YAŞA on 30.03.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var priceTextField: UITextField!
+    @IBOutlet var sizeTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clickedSaveButton(_ sender: Any) {
+    }
+    
+}
--- ShoppingList2/ViewController.swift
+++ ShoppingList2/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
