1afe6479e18d5bebb7addbf8555382c27c917d34
Tuba Nur <yasatubanur@gmail.com>
2022-03-31 12:20:39 +0300

    getting data coded

--- ShoppingList2/ViewController.swift
+++ ShoppingList2/ViewController.swift
@@ -6,18 +6,63 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var tableView: UITableView!
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(clickedAddButton))
+        
+        getData()
+    }
+    
+    func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Shopping")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject]{
+                if let name = result.value(forKey: "name") as? String{
+                    nameArray.append(name)
+                }
+                if let id = result.value(forKey: "id") as? UUID{
+                    idArray.append(id)
+                }
+            }
+            
+            tableView.reloadData()
+        }catch{
+            print("error")
+        }
     }
 
     @objc func clickedAddButton() {
         performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
     }
 
 }
