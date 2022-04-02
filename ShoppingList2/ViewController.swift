5214f9e8eacd0ebcd1d2ac6b9803691078c5657e
Tuba Nur <yasatubanur@gmail.com>
2022-04-02 09:29:03 +0300

    deleting product done

--- ShoppingList2/ViewController.swift
+++ ShoppingList2/ViewController.swift
@@ -89,5 +89,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         chosenUUID = idArray[indexPath.row]
         performSegue(withIdentifier: "toDetailVC", sender: nil)
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Shopping")
+            let uuidString = idArray[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@ ", uuidString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey: "id") as? UUID{
+                            if id == idArray[indexPath.row]{
+                                context.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                
+                                self.tableView.reloadData()
+                                
+                                do{
+                                    try context.save()
+                                }catch{
+                                    print("save error !")
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+            }catch{
+                
+            }
+        }
+    }
 }
 
